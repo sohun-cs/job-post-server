3aa28e23c059a2ae1a15f38b21076d94f273d3cd
sohun-cs <asaduzzamansohun5@gmail.com>
2024-09-23 16:53:10 +0600

    Implemented post, put, delete method

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const coreOptions = {
-    origin: ['http://localhost:5173', 'http://localhost:5174'],
-    Credential: true,
-    optionSuccessStatus: 200
+  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  Credential: true,
+  optionSuccessStatus: 200
 }
 
 app.use(cors(coreOptions));
@@ -30,15 +30,63 @@ const client = new MongoClient(uri, {
 
 async function run() {
 
-    const jobCollection = client.db("jobPostDB").collection("jobs");
+  const jobCollection = client.db("jobPostDB").collection("jobs");
+  const bidCollection = client.db("jobPostDB").collection("bids");
 
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // Send a ping to confirm a successful connection
 
-    app.get('/jobs', async(req, res) => {
-        const result = await jobCollection.find().toArray();
-        res.send(result)
+    app.get('/jobs', async (req, res) => {
+      const result = await jobCollection.find().toArray();
+      res.send(result)
+    });
+
+    app.get('/job/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await jobCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.get('/jobs/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { 'buyer.email': email };
+      const result = await jobCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.post('/jobs', async (req, res) => {
+      const jobData = req.body;
+      const result = await jobCollection.insertOne(jobData);
+      res.send(result);
+    });
+
+    app.delete('/job/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await jobCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    app.post('/bids', async (req, res) => {
+      const bidData = req.body;
+      const result = await bidCollection.insertOne(bidData);
+      res.send(result);
+    });
+
+    app.put('/job/:id', async (req, res) => {
+      const id = req.params.id;
+      const jobData = req.body;
+      const query = { _id: new ObjectId(id) };
+      const option = { upsert: true };
+      const updateDoc = {
+        $set: {
+          ...jobData,
+        },
+      }
+      const result = await jobCollection.updateOne(query, updateDoc, option);
+      res.send(result);
     })
 
     await client.db("admin").command({ ping: 1 });
@@ -53,9 +101,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send("Hello Job post server")
+  res.send("Hello Job post server")
 });
 
 app.listen(port, () => {
-    console.log(`Server is Running on ${port}`);
+  console.log(`Server is Running on ${port}`);
 })
