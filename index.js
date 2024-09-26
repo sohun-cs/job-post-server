84de106965c20db457e2764b791531dc1445ab45
sohun-cs <asaduzzamansohun5@gmail.com>
2024-09-26 19:43:24 +0600

    Added jwt in backend

--- index.js
+++ index.js
@@ -2,20 +2,45 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const port = process.env.PORT || 5000;
 const app = express();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
-const coreOptions = {
-  origin: ['http://localhost:5173', 'http://localhost:5174'],
-  Credential: true,
-  optionSuccessStatus: 200
+const corsOptions = {
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:5174',
+  ],
+  credentials: true,
+  optionSuccessStatus: 200,
 }
-
-app.use(cors(coreOptions));
+app.use(cors(corsOptions));
 app.use(express.json());
+app.use(cookieParser());
+
+// verify jwt middleware
+const verifyToken = (req, res, next) => {
+  const token = req.cookies?.token;
+  console.log(token);
+
+  if (!token) return res.status(401).send({ message: 'unauthorized access' });
+
+  if (token) {
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+      if (err) {
+        return res.status(401).send({ message: 'unauthorized access' });
+      }
+      console.log(decoded);
+
+      req.user = decoded;
+      next();
+
+    })
+  }
 
 
+}
 
 
 const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.k7dzav4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -40,13 +65,31 @@ async function run() {
 
 
     // JWT generate
-    app.post('/jwt', async(req, res) => {
-      const user = req.body;
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+    app.post('/jwt', async (req, res) => {
+      const email = req.body;
+      const token = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET, {
         expiresIn: '10d'
       });
-      res.send({token})
-    })
+      res
+        .cookie('token', token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === 'production',
+          sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict'
+        })
+        .send({ success: true });
+    });
+
+    // Clear token on logout
+    app.get('/logout', (req, res) => {
+      res
+        .clearCookie('token', {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === 'production',
+          sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+          maxAge: 0,
+        })
+        .send({ success: true })
+    });
 
 
 
@@ -55,15 +98,22 @@ async function run() {
       res.send(result)
     });
 
-    app.get('/job/:id', async (req, res) => {
+    app.get('/job/:id', verifyToken, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await jobCollection.findOne(query);
       res.send(result);
     });
 
-    app.get('/jobs/:email', async (req, res) => {
+    // --------------------------------------------------------------
+    app.get('/jobs/:email', verifyToken, async (req, res) => {
+      const tokenEmail = req.user.email;
       const email = req.params.email;
+
+      if (tokenEmail !== email) {
+        return res.status(403).send({ message: 'forbidden access' });
+      }
+
       const query = { 'buyer.email': email };
       const result = await jobCollection.find(query).toArray();
       res.send(result);
@@ -75,14 +125,14 @@ async function run() {
       res.send(result);
     });
 
-    app.delete('/job/:id', async (req, res) => {
+    app.delete('/job/:id', verifyToken, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await jobCollection.deleteOne(query);
       res.send(result);
     });
 
-    app.put('/job/:id', async (req, res) => {
+    app.put('/job/:id', verifyToken, async (req, res) => {
       const id = req.params.id;
       const jobData = req.body;
       const query = { _id: new ObjectId(id) };
@@ -103,14 +153,14 @@ async function run() {
       res.send(result);
     });
 
-    app.get('/my-bids/:email', async (req, res) => {
+    app.get('/my-bids/:email', verifyToken, async (req, res) => {
       const email = req.params.email;
       const query = { email: email };
       const result = await bidCollection.find(query).toArray();
       res.send(result);
     });
 
-    app.get('/bid-requests/:email', async (req, res) => {
+    app.get('/bid-requests/:email', verifyToken, async (req, res) => {
       const email = req.params.email;
       const query = { 'buyer.email': email }
       const result = await bidCollection.find(query).toArray();
